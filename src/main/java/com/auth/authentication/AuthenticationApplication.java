534e2436d10a116121d416a3151f92f91641e719
Jay Raval <jayraval77@gmail.com>
2019-12-26 14:00:09 -0500

    pdated code

--- src/main/java/com/auth/authentication/AuthenticationApplication.java
+++ src/main/java/com/auth/authentication/AuthenticationApplication.java
@@ -2,9 +2,10 @@ package com.auth.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AuthenticationApplication {
+public class AuthenticationApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationApplication.class, args);
